54e41e085c9099aa61ce5901b91a3a160c60d781
Strangework <nicholas.yang.chee@gmail.com>
2014-12-27 23:11:41 -0800

    Collinear points from graham scan removed.

--- sigilizer.py
+++ sigilizer.py
@@ -1,8 +1,8 @@
 import sys
 import svgwrite
 
-A = [(0,0), (1, 2), (2,0)]
-B = [(0,2), (1, 0), (2,2)]
+A = [(0,0), (1, 3), (2,0)]
+B = [(0,3), (1, 0), (2,3)]
 
 if (len(sys.argv) > 1):
   DEBUG = bool(int(sys.argv[1]))
@@ -13,17 +13,17 @@ SVG_WIDTH = 400
 SVG_HEIGHT = 400
 SVG_RATIO = 0.8
 TRUE_NORM = True
-LINE_THICKNESS = 10
+LINE_THICKNESS = 15
 
 def center_shape(point_list, width, height):
-'''
-Changes coordinates in a list of points to be centered within a given width and height.
+  '''
+  Changes coordinates in a list of points to be centered within a given width and height.
 
-Input:	point_list - a list of points describing the shape
-	width - width of intended space
-	height - height of intended space
-Output: a list of points centered about the given dimensions
-'''
+  Input:	point_list - a list of points describing the shape
+    width - width of intended space
+    height - height of intended space
+  Output: a list of points centered about the given dimensions
+  '''
   center_x = (max(point_list, key=lambda x: x[0])[0] + min(point_list, key=lambda x: x[0])[0])/2
   center_y = (max(point_list, key=lambda x: x[1])[1] + min(point_list, key=lambda x: x[1])[1])/2
   
@@ -34,17 +34,17 @@ Output: a list of points centered about the given dimensions
   return point_list
 
 def normalize_shape(point_list, width, height, size_ratio, true_norm):
-'''
-Scales a shape such that it fits properly in the canvas. The amount of 'padding' can be set.
-The user decides if the shape will be 'truly' normalized or not. True normalization maintains the height/width ratio of the shape, false normalization fills the canvas as best it can.
+  '''
+  Scales a shape such that it fits properly in the canvas. The amount of 'padding' can be set.
+  The user decides if the shape will be 'truly' normalized or not. True normalization maintains the height/width ratio of the shape, false normalization fills the canvas as best it can.
 
-Input:	point_list - list of points describing the intended shape
-	width - width of canvas
-	height - height of canvas
-	size_ratio - float from 0 - 1 describing how much of the space will be used (e.g. a value of 0.8 will keep 0.1 of its width and height empty on all sides.
-	true_norm - bool selecting true or false normalization
-Output: A list of points normalized according to specifications
-'''
+  Input:	point_list - list of points describing the intended shape
+    width - width of canvas
+    height - height of canvas
+    size_ratio - float from 0 - 1 describing how much of the space will be used (e.g. a value of 0.8 will keep 0.1 of its width and height empty on all sides.
+    true_norm - bool selecting true or false normalization
+  Output: A list of points normalized according to specifications
+  '''
   h_span = max(point_list, key=lambda x: x[0])[0] - min(point_list, key=lambda x: x[0])[0]
   v_span = max(point_list, key=lambda x: x[1])[1] - min(point_list, key=lambda x: x[1])[1]
   h_mult = (width*size_ratio)/h_span;
@@ -60,15 +60,15 @@ Output: A list of points normalized according to specifications
   return center_shape(point_list, width, height)
 
 def draw_shape(svg, thickness, color, point_list):
-'''
-Draws the shape into a given svg.
+  '''
+  Draws the shape into a given svg.
 
-Input:	svg - the given svg file to change
-	thickness - thickness of lines
-	color - intended color of given shape
-	point_list - list of points describing shape
-Output: N/A
-'''
+  Input:	svg - the given svg file to change
+    thickness - thickness of lines
+    color - intended color of given shape
+    point_list - list of points describing shape
+  Output: N/A
+  '''
   for n in range(0, len(point_list)):
     ax = point_list[n][0]
     ay = point_list[n][1]
@@ -86,17 +86,17 @@ Output: N/A
     svg.add(svg.circle(center=point_list[n], r=thickness/2, fill=circ_color))
 
 def mink_sum(a_list, b_list):
-'''
-Accepts two lists of points and produces its minkowski sum.
-The resultant list does not imply a hull!!
+  '''
+  Accepts two lists of points and produces its minkowski sum.
+  The resultant list does not imply a hull!!
 
-Input:	a_list - some list of points A
-	b_list - some list of points B
-Output  The minkowski sum of A and B
+  Input:	a_list - some list of points A
+    b_list - some list of points B
+  Output  The minkowski sum of A and B
 
-DEVSTATUS:
-	The resultant list does NOT imply a hull yet!! This must be implemented!
-'''
+  DEVSTATUS:
+    The resultant list does NOT imply a hull yet!! This must be implemented!
+  '''
   ret_list = []
   for i in a_list:
     for j in b_list:
@@ -105,79 +105,109 @@ DEVSTATUS:
 
 
 def flatten_shape(some_list, f):
-'''
-Accepts a list of points and returns a 'functional' version.
-Functional means that for each x coord, there is only one corresponding y coord.
-If there are several points that share an x coord, one point is selected using a given comparator.
+  '''
+  Accepts a list of points and returns a 'functional' version.
+  Functional means that for each x coord, there is only one corresponding y coord.
+  If there are several points that share an x coord, one point is selected using a given comparator.
 
-Input:	some_list - an x-ordered list of coordinates
-	f - a functor accepting two ints, returning bool
-Output: A flattened list of 'functional' points.
+  Input:	some_list - an x-ordered list of coordinates
+    f - a functor accepting two ints, returning bool
+  Output: A flattened list of 'functional' points.
 
-DEVSTATUS:
-	Currently generates a dictionary corresponding with the input set. Keys are x coords, values are lists of associated points.
-	Function should step through each value and select a single point using the given comparator.
-'''
+  DEVSTATUS:
+    Currently generates a dictionary corresponding with the input set. Keys are x coords, values are lists of associated points.
+    Function should step through each value and select a single point using the given comparator.
+  '''
+  ret_list = []
   temp_dict = {}
+
   for n in range(0, len(some_list)):
     if some_list[n][0] not in temp_dict.keys():
       temp_dict[some_list[n][0]] = []
     temp_dict[some_list[n][0]].append(some_list[n])
-#
-#  for k in temp_dict.keys():
-#    list(map(f, 
-  return temp_dict
 
-def graham_scan(x):
-'''
-Graham scan implementation used to find the convex hull of a given set of points
+  for k in temp_dict.keys():
+    best_point = temp_dict[k][0]
+    for p in temp_dict[k]:
+      if (not f(best_point, p)):
+        best_point = p
+    ret_list.append(best_point)    
 
-Input:	x - a list of points
-Output:	A list of points describing the convex hull. The points starts at the left-most point, reaches around the bottom portion to the right-most point, then back along the top portion.
+  ret_list.sort(key=lambda x: x[0])
+  return ret_list
 
-DEVSTATUS:
-	Convex hull contains collinear points; no bueno. This comes from the fact that this graham scan does not shoot for the highest of lowest point on a vertical line.
-	'x' is a shitty variable name.
+def graham_scan(orig_point_list):
+  '''
+  Graham scan implementation used to find the convex hull of a given set of points
 
-'''
-  x.sort(key=lambda x: x[0])
+  Input:	orig_point_list - a list of points
+  Output:	A list of points describing the convex hull. The points starts at the left-most point, reaches around the bottom portion to the right-most point, then back along the top portion.
+
+  DEVSTATUS:
+    Removal of collinear points success!! Shapes sometimes get knocked out of whack, however. Will have to investigate that..
+  '''
   
   #Assemble bottom half
+  point_list = flatten_shape(orig_point_list, lambda x, y: x > y)
+  
   bottom_half = []
-  bottom_half.append(x[0])
+  bottom_half.append(point_list[0])
 
   print("Bottom half:")
-  for n in range(1, len(x)):
+  for n in range(1, len(point_list)):
     #(x2-x1)(y3-y1)-(y2-y1)(x3-x1)
-    if (n < len(x)-1):
-      dir = (x[n][0]-bottom_half[-1][0])*(x[n+1][1]-bottom_half[-1][1]) - (x[n][1]-bottom_half[-1][1])*(x[n+1][0]-bottom_half[-1][0])
-      print("x1: " +str(bottom_half[-1][0])+ " y1: " +str(bottom_half[-1][1])+ ", " + "x2: " +str(x[n][0])+ " y2: " +str(x[n][1])+ ", " + "x3: " +str(x[n+1][0])+ " y3: " +str(x[n+1][1])+ ", "+str(dir), end=" ")
+    if (n < len(point_list)-1):
+      dir = (point_list[n][0]-bottom_half[-1][0])*(point_list[n+1][1]-bottom_half[-1][1]) - (point_list[n][1]-bottom_half[-1][1])*(point_list[n+1][0]-bottom_half[-1][0])
+      print("x1: " +str(bottom_half[-1][0])+ " y1: " +str(bottom_half[-1][1])+ ", " + "x2: " +str(point_list[n][0])+ " y2: " +str(point_list[n][1])+ ", " + "x3: " +str(point_list[n+1][0])+ " y3: " +str(point_list[n+1][1])+ ", "+str(dir), end=" ")
+      if (dir < 0):
+        if (len(bottom_half) > 1):
+          dir = (bottom_half[-1][0]-bottom_half[-2][0])*(point_list[n][1]-bottom_half[-2][1]) - (bottom_half[-1][1]-bottom_half[-2][1])*(point_list[n][0]-bottom_half[-2][0])
+          print("BREH: " +str(dir))
+          if (dir == 0):
+            bottom_half.pop()
+        bottom_half.append(point_list[n])
+        print("[APPENDED]")
+      else:
+        print("")
     else:
-      dir = (x[n][0]-bottom_half[-1][0])*(bottom_half[0][1]-bottom_half[-1][1]) - (x[n][1]-bottom_half[-1][1])*(bottom_half[0][0]-bottom_half[-1][0])
-      print("x1: " +str(bottom_half[-1][0])+ " y1: " +str(bottom_half[-1][1])+ ", " + "x2: " +str(x[n][0])+ " y2: " +str(x[n][1])+ ", " + "x3: " +str(bottom_half[0][0])+ " y3: " +str(bottom_half[0][1])+ ", "+str(dir), end=" ")
-    if (dir < 0):
-      bottom_half.append(x[n])
-      print("[APPENDED]")
-    else:
-      print("")
+      if (len(bottom_half) > 1):
+        dir = (bottom_half[-1][0]-bottom_half[-2][0])*(point_list[-1][1]-bottom_half[-2][1]) - (bottom_half[-1][1]-bottom_half[-2][1])*(point_list[-1][0]-bottom_half[-2][0])
+        print("BREH: " +str(dir))
+        if (dir == 0):
+          bottom_half.pop()
+      bottom_half.append(point_list[-1])
+      print("x: " +str(bottom_half[-1][0])+ " y: " +str(bottom_half[-1][-1])+ " [APPENDED]")
   print(str(bottom_half) + "\n")
 
   #Assemble top half
+  point_list = flatten_shape(orig_point_list, lambda x, y: x < y)
+
   top_half = []
   top_half.append(bottom_half[-1])
   print("Top half:")
-  for n in reversed(range(1, len(x))):
+  for n in reversed(range(0, len(point_list))):
     #(x2-x1)(y3-y1)-(y2-y1)(x3-x1)
     if (n > 0):
-      dir = (x[n][0]-top_half[-1][0])*(x[n-1][1]-top_half[-1][1]) - (x[n][1]-top_half[-1][1])*(x[n-1][0]-top_half[-1][0])
+      dir = (point_list[n][0]-top_half[-1][0])*(point_list[n-1][1]-top_half[-1][1]) - (point_list[n][1]-top_half[-1][1])*(point_list[n-1][0]-top_half[-1][0])
+      print("x: " +str(point_list[n][0])+ " y: " +str(point_list[n][1])+ ", " +str(dir), end=" ")
+      if (dir < 0):
+        if (len(top_half) > 1):
+          dir = (top_half[-1][0]-top_half[-2][0])*(point_list[n][1]-top_half[-2][1]) - (top_half[-1][1]-top_half[-2][1])*(point_list[n][0]-top_half[-2][0])
+          if (dir == 0):
+            top_half.pop()
+        top_half.append(point_list[n])
+        print("[APPENDED]")
+      else:
+        print("")
     else:
-      dir = (x[n][0]-top_half[-1][0])*(bottom_half[0][1]-top_half[-1][1]) - (x[n][1]-top_half[-1][1])*(bottom_half[0][0]-top_half[-1][0])
-    print("x: " +str(x[n][0])+ " y: " +str(x[n][1])+ ", " +str(dir), end=" ")
-    if (dir < 0):
-      top_half.append(x[n])
-      print("[APPENDED]")
-    else:
-      print("")
+      if (len(top_half) > 1):
+        dir = (top_half[-1][0]-top_half[-2][0])*(bottom_half[0][1]-top_half[-2][1]) - (top_half[-1][1]-top_half[-2][1])*(bottom_half[0][0]-top_half[-2][0])
+        print("BREH: " +str(dir))
+        if (dir == 0):
+          top_half.pop()
+      print("x: " +str(top_half[-1][0])+ " y: " +str(top_half[-1][-1])+ " [APPENDED]")
+  top_half.pop(0)
+
   print(str(top_half) + "\n")
 
   return(bottom_half + top_half)
@@ -191,14 +221,14 @@ def main():
   a_norm = normalize_shape(A, SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
   b_norm = normalize_shape(B, SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
   c_norm = normalize_shape(graham_scan(mink_sum(a_norm, b_norm)), SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
-  #d_norm = normalize_shape(graham_scan(mink_sum(c_norm, a_norm)), SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
+  d_norm = normalize_shape(graham_scan(mink_sum(c_norm, a_norm)), SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
   e_norm = normalize_shape(graham_scan(mink_sum(c_norm, b_norm)), SVG_WIDTH, SVG_HEIGHT, SVG_RATIO, TRUE_NORM)
 
-  #draw_shape(svg, LINE_THICKNESS, "#FFA500", d_norm)
+  draw_shape(svg, LINE_THICKNESS, "#FFA500", d_norm)
   draw_shape(svg, LINE_THICKNESS, "#FFA500", e_norm)
-  #draw_shape(svg, LINE_THICKNESS, "#708090", c_norm)
-  #draw_shape(svg, LINE_THICKNESS, "#3CB371", a_norm)
-  #draw_shape(svg, LINE_THICKNESS, "#3CB371", b_norm)
+  draw_shape(svg, LINE_THICKNESS, "#708090", c_norm)
+  draw_shape(svg, LINE_THICKNESS, "#3CB371", a_norm)
+  draw_shape(svg, LINE_THICKNESS, "#3CB371", b_norm)
 
   svg.save()
 
